311c8e495b5cbaee967216f4053b51429183cc7c
Ivan Perehiniak <iv.perehinik@gmail.com>
2025-10-14 20:22:03 -0500

    Add python3-evdev as a dependency.

--- build.sh
+++ build.sh
@@ -5,11 +5,7 @@ cd "$(dirname "$0")"
 set -e
 
 mkdir -p ./build
-mkdir -p ./mp-lib
 rm -rf ./build/*
-rm -rf ./mp-lib/*
-
-pip install evdev --target ./mp-lib
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
